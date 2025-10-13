4ba2f435fd990cc365cf43a02570419792e0ee07
amarquezmazzeo <amarquezmazz@mail.usf.edu>
2025-10-13 00:14:39 -0400

    setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/amarquezmazzeo/bootdev-go-server
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("something happened: %v", err)
+	}
+}
