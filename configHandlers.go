f3cbe53a83ead5fe3e6e06c9fbf9533bfd4caada
amarquezmazzeo <amarquezmazz@mail.usf.edu>
2025-10-14 23:30:05 -0400

    metrics handler returns html

--- configHandlers.go
+++ configHandlers.go
@@ -18,8 +18,15 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 	currentCount := cfg.fileserverHits.Load()
-	fmt.Fprintf(w, "Hits: %d", currentCount)
+	fmt.Fprintf(w, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, currentCount)
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
