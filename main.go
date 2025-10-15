05ccfebf0568f0611c7b76690082e8d43e6b522e
amarquezmazzeo <amarquezmazz@mail.usf.edu>
2025-10-14 22:27:56 -0400

    route and method changes for healthz, metrics and reset enpoints

--- main.go
+++ main.go
@@ -10,9 +10,9 @@ func main() {
 	mux := http.NewServeMux()
 	apiCfg := &apiConfig{}
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("/healthz", handlerHealth)
-	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("GET /api/healthz", handlerHealth)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
